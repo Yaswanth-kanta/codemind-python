db00b933908c6552b372be0907a78cc60692ac5c
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-06-21 10:08:32 +0530

    def pri(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True a=int(input()) b=int(input()) for i in range(a,b+1):     if pri(i):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,14 +1,13 @@
-def prime(n):
-    c=0
-    for i in range(1,n+1):
-        if n%i==0:
-            c+=1
-    if c==2:
-        return True
-    else:
+def pri(n):
+    if n==1:
         return False
-n1=int(input())
-n2=int(input())
-for i in range(n1,n2+1):
-    if prime(i):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if pri(i):
         print(i)
