7a765a81d2a8f90863d7cea1861d7629ed70fd68
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-07-01 10:51:28 +0530

    def pri(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) a=list(map(int,input().split())) c=0 for i in a:     if pri(i):         c+=1 print(c)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,16 @@
+def pri(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in a:
+    if pri(i):
+        c+=1
+print(c)
+        
