f34a4b35ecf1c0e2f395d3aae53d201e407d98dc
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-07-05 20:54:34 +0530

    def pri(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) a=list(map(int,input().split())) k=int(input()) c=0 for i in a:     if i<=k and pri(i):         c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,16 @@
+def pri(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+a=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in a:
+    if i<=k and pri(i):
+        c+=1
+print(c)
