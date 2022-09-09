58107ca7f4983ebba17617d7a4b7fa76c2eec616
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-09 23:28:03 +0530

    for i in range(int(input())):     n,m=map(int,input().split())     a=list(map(int,input().split()))     b=list(map(int,input().split()))     c=a+b     c.sort()     print(*c)

---
+++ Merge_Without_Extra_Space.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    n,m=map(int,input().split())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    c=a+b
+    c.sort()
+    print(*c)
+    
