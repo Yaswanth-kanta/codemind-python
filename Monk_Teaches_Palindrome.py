fa8cb9af02f8431f7a42152bfaa87b21ddd013fa
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-10-08 15:52:52 +0530

    for i in range(int(input())):     s=input()     if s==s[::-1]:         if len(s)%2==0:             print("YES EVEN")         else:             print("YES ODD")     else:         print("NO")

---
+++ Monk_Teaches_Palindrome.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    s=input()
+    if s==s[::-1]:
+        if len(s)%2==0:
+            print("YES EVEN")
+        else:
+            print("YES ODD")
+    else:
+        print("NO")
